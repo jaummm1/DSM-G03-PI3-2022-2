deef24c62cd9a26496d41a16d8ffac63b5ff95d6
jaummm1 <88463022+jaummm1@users.noreply.github.com>
2022-10-19 15:40:27 -0300

    adicionando as rotas no app.js

--- back-end/app.js
+++ back-end/app.js
@@ -2,19 +2,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+/* Rotas da aplicação */
+const userRoute = require('./routes/user')
+app.use('/user', userRoute)
+
+const criterionRoute = require('./routes/criterion')
+app.use('/criterion', criterionRoute)
+
+const questionRoute = require('./routes/question')
+app.use('/question', questionRoute)
+
+const glossaryRoute = require('./routes/glossary')
+app.use('/glossary', glossaryRoute)
+
+const assessmentRoute = require('./routes/assessment')
+app.use('/assessment', assessmentRoute)
 
 module.exports = app;
